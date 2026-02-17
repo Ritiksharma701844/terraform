56f20e27dde00d61d973724e7fbdf80ad814825a
Ritiksharma701844 <165645294+Ritiksharma701844@users.noreply.github.com>
2026-02-18 01:37:00 +0530

    new var file

--- variable.tf
+++ variable.tf
@@ -1,5 +1,11 @@
+variable "cluster_name" {
+  default = "megaproject-eks"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
-  type        = string
-  default     = "megaproject"
+  default = "megaproject"
 }
